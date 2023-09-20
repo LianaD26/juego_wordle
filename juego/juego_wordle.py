ba25f1ed9213df86e26d081250f9249da6f71b86
LianaD26 <limadio.uni@gmail.com>
2023-09-20 15:51:52 -0500

    fourth commit

--- juego/juego_wordle.py
+++ juego/juego_wordle.py
@@ -1,64 +1,80 @@
 import random
-class Wordle:
 
+class Wordle:
     def seleccionar_palabra(self) -> str:
         with open("lista_palabras.txt", "r") as archivo:
             lineas = archivo.readlines()
-            lista = []
-            for linea in lineas:
-                palabra = linea.strip()  # sin saltos de línea
-                lista.append(palabra)
+            lista = [linea.strip() for linea in lineas]
         num_pal = random.randint(0, len(lista) - 1)
         pal = lista[num_pal]
         return pal
+    def ejecutar_juego(self, wordle):
+        tablero = Tablero()
+        palabra_correcta = wordle.seleccionar_palabra()
+        while tablero.num_intentos < 6:
+            tablero.imprimir_tablero()
+            tablero.ingresar_palabra(palabra_correcta)
+            tablero.colorear_matriz(palabra_correcta)
+            if "".join(tablero.matriz[tablero.num_intentos-1]) == palabra_correcta:
+                print("Has adivinado la palabra!")
+                break
+
+        tablero.imprimir_tablero()
+        print(f"¡Agotaste tus intentos! La palabra correcta era: {palabra_correcta}")
+
 class Color:
     def __init__(self):
         self.GRIS = "\033[90m"
         self.VERDE = "\033[92m"
         self.AMARILLO = "\033[93m"
-        self.RESET =  "\033[0m"
+        self.RESET = "\033[0m"
+
 class Tablero:
     def __init__(self):
         self.num_intentos: int = 0
         self.matriz = []
         self.llenar_matriz()
+
     def llenar_matriz(self):
         for i in range(6):
-            self.matriz.append(["_" for e in range(5)])
-    def ingresar_palabra(self):
-        if self.num_intentos <= 6:
-            for m in range(5):
-                e = input("letra: ")
-                if e.isalpha() and len(e) == 1:
-                    #self.matriz[self.num_intentos][m].append(e)
-                    self.matriz[self.num_intentos][m] = e
+            self.matriz.append(["_" for _ in range(5)])
 
-            self.num_intentos += 1
-            for i in range(6):
-                for j in range(5):
-                    print(self.matriz[i][j])
-        else:
-            for i in range(6):
-                for j in range(5):
-                    print(self.matriz[i][j])
+    def imprimir_tablero(self):
+        for fila in self.matriz:
+            print(" ".join(fila))
+        print(f"Intento {self.num_intentos}/6")
 
-    def colorear_matriz(self):
-        wordle = Wordle()
-        for j in range(5):
-            letra = self.matriz[self.num_intentos][j]
-            palabra_correcta = wordle.seleccionar_palabra()
-            if letra in palabra_correcta and letra[j] == palabra_correcta[j]:
-                self.matriz[self.num_intentos][j] = f"{Color().VERDE}letra{Color().RESET}"
-            elif letra in palabra_correcta and letra[j] != palabra_correcta[j]:
-                self.matriz[self.num_intentos][j] = f"{Color().AMARILLO}letra{Color().RESET}"
+    def ingresar_palabra(self, palabra_correcta):
+        if self.num_intentos < 6:
+            print("Ingresa una palabra de 5 letras en minúsculas:")
+            palabra = input()
+            if len(palabra) == 5 and palabra.isalpha() and palabra.islower():
+                if palabra == palabra_correcta:
+                    for i, letra in enumerate(palabra):
+                        self.matriz[self.num_intentos][i] = letra
+                    self.num_intentos = 6
+                    return
+                else:
+                    for i, letra in enumerate(palabra):
+                        self.matriz[self.num_intentos][i] = letra
+                    self.num_intentos += 1
             else:
-                self.matriz[self.num_intentos][j] = f"{Color().GRIS}letra{Color().RESET}"
+                print("Por favor, ingresa una palabra válida de 5 letras en minúsculas.")
+        else:
+            print("Ya has alcanzado el límite de intentos.")
 
-        for i in range(6):
-            for j in range(5):
-                print(self.matriz[i][j])
+    def colorear_matriz(self, palabra_correcta):
+        nueva_lista = palabra_correcta
+        for j in range(5):
+            letra = self.matriz[self.num_intentos - 1][j]
+            if letra in nueva_lista and letra != nueva_lista[j]:
+                self.matriz[self.num_intentos - 1][j] = f"{Color().AMARILLO}{letra}{Color().RESET}"
+                nueva_lista = nueva_lista.replace(letra, "-")
+            elif letra == nueva_lista[j]:
+                self.matriz[self.num_intentos - 1][j] = f"{Color().VERDE}{letra}{Color().RESET}"
+                nueva_lista = nueva_lista.replace(letra, "-")
+            else:
+                self.matriz[self.num_intentos - 1][j] = f"{Color().GRIS}{letra}{Color().RESET}"
 
-wordle = Wordle()  # Crear una instancia de Wordle
-palabra_correcta = wordle.seleccionar_palabra()  # Llamar al método en la instancia
-tablero = Tablero()
-tablero.ingresar_palabra()
+wordle = Wordle()
+wordle.ejecutar_juego(wordle)
--- juego/prueba.py
+++ juego/prueba.py
@@ -1,11 +1,3 @@
-class Color:
-    def __init__(self):
-        self.GRIS = "\033[90m"
-        self.VERDE = "\033[92m"
-        self.AMARILLO = "\033[93m"
-        self.RESET =  "\033[0m"
-l = "a"
-print(l)
-a = f"{Color().VERDE}l{Color().RESET}"
-print(a)
-print(l,a)
+txt = "I like bananas"
+txt = txt.replace("bananas", "apples")
+print(txt)
